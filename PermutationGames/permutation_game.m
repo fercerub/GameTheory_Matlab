3341d4311a2540718bdd1839bb33cc3a0dd5022d
LAPTOP-8MH55NU9\natafer <fernandocerveraubeda@gmail.com>
2022-01-17 05:03:45 +0100

    Subir permutationGames

---
+++ PermutationGames/permutation_game.m
@@ -0,0 +1,53 @@
+function v = permutation_game(A)
+[filas, columnas] = size(A);
+if filas ~= columnas
+    error('Wrong matrix');
+end
+
+v = zeros(1, 2^filas-1);
+
+subsets = generateOrderedSubsets(filas);
+
+for index_coalition = 2:length(subsets)    
+    v(index_coalition-1) = calculate_savings(A, subsets(index_coalition));
+end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function savings = calculate_savings(A, subset)
+
+original_cost = sum(A(sub2ind(size(A),subset.players,subset.players)));
+
+permutation_set = perms(subset.players);
+num_permutations = factorial(length(subset.players));
+min_cost = zeros(1, num_permutations);
+
+for i=1:num_permutations
+    min_cost(i) = sum(A(sub2ind(size(A),subset.players,permutation_set(i,:))));
+end
+
+savings = original_cost - min(min_cost);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function subsets = generateOrderedSubsets(n)
+subsets = [];
+decimalExpression = [];
+
+allSubsets = fliplr(dec2bin(0:2^n-1)-'0');
+
+for i=1:n
+    allSubsets(:,i) = i*allSubsets(:,i);
+end
+
+for i=2:2^n
+    decimalExpression = [decimalExpression str2double(sprintf('%d',allSubsets(i, allSubsets(i,:)~=0)))];    
+end
+decimalExpression = [0 sort(decimalExpression)];
+
+for i=1:2^n
+    subsets(i).players = num2str(decimalExpression(i))-'0';
+end
+subsets(1).players = [];
+end
